4d30c56fe6cde856fc48179854fc28f0d69bf698
Mikkel Madsen <mikkelmadsen@mikkels-air.home>
2019-02-14 16:35:12 +0100

    Initial commit, array created runs as planned. boxes are created and added but they aren't removed when there are more than 40

---
+++ script.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const arr = [];
+
+let counter = 0;
+
+init();
+
+function init() {
+  // add 39 "0´s" to the array
+
+  counter++;
+
+  const maxLengthOfArray = 40;
+
+  if (counter <= maxLengthOfArray) {
+    arr.unshift(0);
+    init();
+  } else {
+    console.log(arr);
+    addRandomNumToArray();
+  }
+}
+
+function makeRandomNumber(min, max) {
+  const numb = Math.floor(Math.random() * (max - min) + min);
+
+  return numb;
+}
+
+function addRandomNumToArray() {
+  counter++;
+
+  //   random number between 0-35
+  let randomNumber = makeRandomNumber(0, 35);
+
+  console.log(randomNumber);
+
+  arr.unshift(randomNumber);
+
+  const LengthOfArray = 40;
+
+  if (counter > LengthOfArray) {
+    arr.pop();
+    createBox();
+  }
+  console.log(arr);
+
+  setTimeout(addRandomNumToArray, 1000);
+}
+
+function createBox() {
+  let height = arr[1];
+
+  //grap html template
+  const clone = document.querySelector("template").content.cloneNode(true);
+  //add height to box from template
+  const box = (clone.querySelector(".box").style.height = height * 3 + "px");
+
+  //   const element = document.querySelector("#container").appendChild(clone);
+
+  document.querySelector("#container").appendChild(clone);
+
+  console.log(box);
+
+  return clone;
+}
